59fcce43e317cda4904465ccbc94991102612c69
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-02 23:54:07 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken(board, index)
   if board[index] != nil || board[index] != " " || board[index] != ""
-    return true
+    return false
   return false
   end
 end
