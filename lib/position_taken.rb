e1cc2f4d517313f29bca6e67dacc3b6ba46b1b36
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-03 00:04:39 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
+  flag = [" ","", nil]
   if board[index] != nil || board[index] != " " || board[index] != ""
     return true
   return false
